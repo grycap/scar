48955d40839fa836884cf3f15474f210d18013d5
alpegon <alpegon3@upvnet.upv.es>
2017-05-18 16:33:51 +0200

    Update command parser with the basic commands needed and the basic functions

--- scar.py
+++ scar.py
@@ -14,26 +14,41 @@ class Scar(object):
                                               epilog="Run 'scar COMMAND --help' for more information on a command.")
         subparsers = self.parser.add_subparsers(title='Commands')
                 
-        # Create the parser for the 'run' command
-        parser_run = subparsers.add_parser('run', help="Deploy function")
-        # Set the positional arguments
-        parser_run.add_argument("container_id", help="Execute container")
-        # Set the optional arguments 
-        parser_run.add_argument("-ak", "--aws_access_key", help="AWS access key")
-        parser_run.add_argument("-sk", "--aws_secret_key", help="AWS secret access key")
+        # 'init' command
+        parser_init = subparsers.add_parser('init', help="Create lambda function")
         # Set default function
+        parser_init.set_defaults(func=self.init)
+        # Set the positional arguments
+        parser_init.add_argument("image_id", help="Container image id (i.e. centos:7)")
+        # Set the optional arguments 
+        parser_init.add_argument("-n", "--name", help="Lambda function name")
+        parser_init.add_argument("-m", "--memory", help="Lambda function memory")
+        parser_init.add_argument("-t", "--time", help="Lambda function maximum execution time")
+    
+        # 'ls' command
+        parser_ls = subparsers.add_parser('ls', help="List lambda functions")
+        parser_ls.set_defaults(func=self.ls)
+        
+        # 'run' command
+        parser_run = subparsers.add_parser('run', help="Deploy function")
         parser_run.set_defaults(func=self.run)
+        parser_run.add_argument("name", help="Lambda function name")
+        parser_run.add_argument("-m", "--memory", help="Lambda function memory")
+        parser_run.add_argument("-t", "--time", help="Lambda function maximum execution time")
+        parser_run.add_argument("--async", help="Tell Scar to wait or not for the lambda function return", action="store_true")
         
         # Create the parser for the 'rm' command
         parser_rm = subparsers.add_parser('rm', help="Delete function")
-        # Set the positional arguments
-        parser_rm.add_argument("container_id")
-        # Set default function
         parser_rm.set_defaults(func=self.rm)
+        parser_rm.add_argument("name", help="Lambda function name")
         
+        # 'log' command
+        parser_log = subparsers.add_parser('log', help="Show the logs for the lambda function")
+        parser_log.set_defaults(func=self.log)
+        parser_log.add_argument("name", help="Lambda function name")       
+
         # Create the parser for the 'version' command
         parser_version = subparsers.add_parser('version', help="Show the Scar version information")
-        # Set default function
         parser_version.set_defaults(func=self.version)        
                          
     def execute(self):
@@ -41,12 +56,21 @@ class Scar(object):
         args = self.parser.parse_args()
         args.func(args)
 
+    def init(self, args):
+        print args
+        
+    def ls(self, args):
+        print args       
+        
     def run(self, args):
-        print "run " + args.container_id
+        print args       
             
     def rm(self, args):
-        print "rm " + args.container_id
-        
+        print args
+
+    def log(self, args):
+        print args
+    
     def version(self, args):
         print "scar " + version
         
