4a36012b89eea100044a6c13704bd0b6f4f6d236
alpegon <alpegon3@upvnet.upv.es>
2017-07-06 15:40:54 +0200

    Update darknet example
    Now the output image is also saved in the s3 bucket

--- examples/darknet/yolo-sample-object-detection.sh
+++ examples/darknet/yolo-sample-object-detection.sh
@@ -2,9 +2,14 @@
 
 OUTPUT_DIR="/tmp/$REQUEST_ID/output"
 FILENAME=`basename $SCAR_INPUT_FILE`
-OUTPUT_FILE="$OUTPUT_DIR/${FILENAME%.*}.out"
-echo "SCRIPT: Analyzing image file $SCAR_INPUT_FILE and saving the output in file $OUTPUT_FILE"
+FILENAME_NO_EXTENSION=${FILENAME%.*}
+
+RESULT="$OUTPUT_DIR/$FILENAME_NO_EXTENSION.out"
+OUTPUT_IMAGE=$FILENAME_NO_EXTENSION-detected
+
+echo "SCRIPT: Analyzing image file $SCAR_INPUT_FILE and saving the output in file $RESULT and image $OUTPUT_IMAGE"
 
 cd /opt/darknet
-./darknet detect cfg/yolo.cfg yolo.weights $SCAR_INPUT_FILE > $OUTPUT_FILE
+./darknet detect cfg/yolo.cfg yolo.weights $SCAR_INPUT_FILE -out $OUTPUT_IMAGE > $RESULT
 
+mv $OUTPUT_IMAGE.png $OUTPUT_DIR/$OUTPUT_IMAGE.png
