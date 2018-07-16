558b253d3e7f88202b73fafcc49a8c654d356e2c
alpegon <alpegon3@upvnet.upv.es>
2018-07-16 11:04:11 +0200

    Update cfg file to add missing parameters

--- src/parser/cfgfile.py
+++ src/parser/cfgfile.py
@@ -17,35 +17,57 @@ import os
 import src.logger as logger
 import shutil
 import json
-
-config_file_folder = os.path.expanduser("~") + "/.scar"
-config_file_name = "scar.cfg"
-config_file_path = config_file_folder + '/' + config_file_name
-default_file_path = os.path.dirname(os.path.realpath(__file__))
+import src.utils as utils
 
 class ConfigFileParser(object):
+    
+    config_file_name = "scar.cfg"
+    config_folder_name = ".scar"
+    default_file_name = "default_config_file.json"
+    config_file_folder = utils.join_paths(os.path.expanduser("~"), config_folder_name)
+    config_file_path = utils.join_paths(config_file_folder, config_file_name)
+    default_file_path = utils.join_paths(os.path.dirname(os.path.realpath(__file__)), default_file_name)    
 
     def __init__(self):
         # Check if the config file exists
-        if os.path.isfile(config_file_path):
-            with open(config_file_path) as cfg_file:
-                self.__setattr__("cfg_data", json.load(cfg_file))  
+        if os.path.isfile(self.config_file_path):
+            with open(self.config_file_path) as cfg_file:
+                self.__setattr__("cfg_data", json.load(cfg_file))
+            if 'region' not in self.cfg_data['aws'] or 'boto_profile' not in self.cfg_data['aws']:
+                self.add_missing_attributes()
         else:
             # Create scar config dir
-            os.makedirs(config_file_folder, exist_ok=True)
+            os.makedirs(self.config_file_folder, exist_ok=True)
             self.create_default_config_file()
         
     def create_default_config_file(self):
-        shutil.copy(default_file_path + "/default_config_file.json", config_file_path)
-        message = "Config file '%s' created.\n" % config_file_path
+        shutil.copy(self.default_file_path, self.config_file_path)
+        message = "Config file '{0}' created.\n".format(self.config_file_path)
         message += "Please, set a valid iam role in the file field 'role' before the first execution."
         logger.warning(message)
         
     def get_properties(self):
         return self.cfg_data
         
-    def get_aws_properties(self):
-        return self.cfg_data['aws']
-        
-        
-        
+    def add_missing_attributes(self):
+        print("Adding missing parameters in config file '{0}'.\nOnly runs once.".format(self.config_file_path))
+        with open(self.default_file_path) as default_file:
+            default_data = json.load(default_file)
+            self.merge_files(self.cfg_data, default_data)
+        self.delete_unused_data()
+        with open(self.config_file_path, mode='w') as cfg_file:
+            cfg_file.write(json.dumps(self.cfg_data, indent=2))
+    
+    def merge_files(self, cfg_data, default_data):
+        for k, v in default_data.items():
+            if k not in cfg_data:
+                cfg_data[k] = v
+            elif type(cfg_data[k]) is dict:
+                self.merge_files(cfg_data[k], default_data[k])
+                
+    def delete_unused_data(self):
+        if 'region' in self.cfg_data['aws']['lambda']:
+            region = self.cfg_data['aws']['lambda'].pop('region', None)
+            if region:
+                self.cfg_data['aws']['region'] = region                             
+            
