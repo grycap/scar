13b7ec68d12b97fda1489424b893b2a5d1ad01b8
alpegon <alpegon3@upvnet.upv.es>
2019-05-27 17:07:02 +0200

    Update examples

--- examples/imagemagick/grayify-image.sh
+++ examples/imagemagick/grayify-image.sh
@@ -23,9 +23,10 @@
 #    automatically uploaded by the Lambda function to the output folder of the S3 bucket.
 #    
 
-echo "SCRIPT: Invoked Image Grayifier. File available in $INPUT_FILE_PATH"
+#echo "SCRIPT: Invoked Image Grayifier. File available in $INPUT_FILE_PATH"
 FILE_NAME=`basename $INPUT_FILE_PATH`
 OUTPUT_FILE=$TMP_OUTPUT_DIR/$FILE_NAME
 
-echo "SCRIPT: Converting input image file $INPUT_FILE_PATH to grayscale to output file $OUTPUT_FILE"
+#echo "SCRIPT: Converting input image file $INPUT_FILE_PATH to grayscale to output file $OUTPUT_FILE"
 convert $INPUT_FILE_PATH -type Grayscale $OUTPUT_FILE
+cat $OUTPUT_FILE
