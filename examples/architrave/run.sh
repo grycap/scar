d38d598ddf53f74e9f59f95cb094256667792975
asalic <asalic@users.noreply.github.com>
2020-09-16 09:25:14 +0200

    rm deps for downloading during batch exec

--- examples/architrave/run.sh
+++ examples/architrave/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Build date: $(cat  /build_date)"
+
 if [ "${EXEC_TYPE,,}" = 'lambda' ]; then
   export OMPI_MCA_plm_rsh_agent=/bin/false
   mpirun ${MPI_PARAMS} ${APP_BIN} ${APP_PARAMS}
@@ -13,35 +15,12 @@ elif [ "${EXEC_TYPE,,}" = 'batch' ]; then
   echo "Running on node index $AWS_BATCH_JOB_NODE_INDEX out of $AWS_BATCH_JOB_NUM_NODES nodes"
   echo "Master node index is $AWS_BATCH_JOB_MAIN_NODE_INDEX and its IP is $AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS"
 
-  #wget -q -P /tmp --no-check-certificate --no-proxy 'http://scar-architrave.s3.amazonaws.com/awscli-exe-linux-x86_64.zip'
-  wget -P /tmp https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
-  echo "Download awscli complete"
-  7z x -aoa -o/tmp/ /tmp/awscli-exe-linux-x86_64.zip
-  chmod +x /tmp/aws/install
-  /tmp/aws/install
-  echo "Version of dist: ${VERSION}"
-  mkdir ~/.aws/
-  ## S3 OPTIMIZATION
-  aws configure set default.s3.max_concurrent_requests 30
-  aws configure set default.s3.max_queue_size 10000
-  aws configure set default.s3.multipart_threshold 64MB
-  aws configure set default.s3.multipart_chunksize 16MB
-  aws configure set default.s3.max_bandwidth 4096MB/s
-  aws configure set default.s3.addressing_style path
-  printf '%s\n' '[default]' "aws_access_key_id=${AWS_ACCESS_KEY}" "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
-  printf '%s\n' '[default]' "region=${AWS_REGION}" "output=${AWS_OUTPUT}" > ~/.aws/config
-  #aws s3 cp $S3_INPUT/common $SCRATCH_DIR
-  echo "Install batch only dependencies from S3"
   mkdir ${SCRATCH_DIR}
   mkdir ${JOB_DIR}
-  aws s3 cp ${S3_BUCKET}/${S3_BATCH_DEPS_REL_PATH} /tmp
-  tar -zxf /tmp/deps.tar.gz -C /tmp
-  dpkg -i /tmp/*.deb
+  dpkg -i ${S3_BATCH_MNT}/deps/*.deb
 
   echo "Add private data from S3"
-  #rm -rf /tmp/*
-  aws s3 cp ${S3_BUCKET}/${S3_BATCH_PRIVATE_REL_PATH} /tmp
-  7z x -aoa -p${PRIVATE_PASSWD} -o/opt /tmp/*.7z
+  7z x -aoa -p${PRIVATE_PASSWD} -o/opt ${S3_BATCH_MNT}/*.7z
 
   echo "Configure ssh"
   sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
