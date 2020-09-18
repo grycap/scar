4ec50d41a70251b483868708b673cf2a324069f5
asalic <asalic@users.noreply.github.com>
2020-09-18 13:05:57 +0200

    added cert to connect between nodes

--- examples/architrave/run.sh
+++ examples/architrave/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Build date: $(cat  /build_date)"
+echo "Runing as: ${USER} home @ ${HOME}"
 
 if [ "${EXEC_TYPE,,}" = 'lambda' ]; then
   export OMPI_MCA_plm_rsh_agent=/bin/false
@@ -10,7 +11,6 @@ elif [ "${EXEC_TYPE,,}" = 'batch' ]; then
 
 # The following comment line will be replaced with the necessary env vars:
 #=ENV_VARS=
-
   export AWS_BATCH_EXIT_CODE_FILE=~/batch_exit_code.file
   echo "Running on node index $AWS_BATCH_JOB_NODE_INDEX out of $AWS_BATCH_JOB_NUM_NODES nodes"
   echo "Master node index is $AWS_BATCH_JOB_MAIN_NODE_INDEX and its IP is $AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS"
@@ -27,19 +27,19 @@ elif [ "${EXEC_TYPE,,}" = 'batch' ]; then
   sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
   echo "export VISIBLE=now" >> /etc/profile
   echo "${USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-  mkdir -p ${SSHDIR}
-  touch ${SSHDIR}/sshd_config
-  ssh-keygen -t rsa -f ${SSHDIR}/ssh_host_rsa_key -N ''
-  cp ${SSHDIR}/ssh_host_rsa_key.pub ${SSHDIR}/authorized_keys
-  cp ${SSHDIR}/ssh_host_rsa_key ${SSHDIR}/id_rsa
-  echo " IdentityFile ${SSHDIR}/id_rsa" >> /etc/ssh/ssh_config
+  mkdir -p ${HOME}/.ssh
+  touch ${HOME}/.ssh/sshd_config
+  #ssh-keygen -t rsa -f ${SSHDIR}/ssh_host_rsa_key -N ''
+  cat /opt/ssh_host_rsa_key.pub > ${HOME}/.ssh/authorized_keys
+  cp /opt/ssh_host_rsa_key  ${HOME}/.ssh/id_rsa
+  echo " IdentityFile ${HOME}/.ssh/id_rsa" >> /etc/ssh/ssh_config
   echo "Host *" >> /etc/ssh/ssh_config
   echo " StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-  chmod -R 600 ${SSHDIR}/*
-  chown -R ${USER}:${USER} ${SSHDIR}/
+  chmod -R 600 ${HOME}/.ssh/*
+  chown -R ${USER}:${USER} ${HOME}/.ssh/
     # check if ssh agent is running or not, if not, run
   eval `ssh-agent -s`
-  ssh-add ${SSHDIR}/id_rsa
+  ssh-add ${HOME}/id_rsa
 
   chmod +x ${APP_BIN}
   service ssh status
