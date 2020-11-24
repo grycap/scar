0063fe08e81c38366034c8de9d816b3f11498d6e
asalic <asalic@users.noreply.github.com>
2020-11-24 13:03:55 +0100

    disable waiting

--- examples/architrave/mpi-run.sh
+++ examples/architrave/mpi-run.sh
@@ -95,11 +95,11 @@ wait_for_nodes () {
   echo "0" > $AWS_BATCH_EXIT_CODE_FILE
   kill  $(cat /tmp/supervisord.pid)
   #echo "#!/bin/bash" > ${S3_BATCH_MNT}/exec/docker_done
-  echo "env GZIP=-9 tar -czvf /mnt/batch/output/result.tar.gz /mnt/batch/output/*" > ${S3_BATCH_MNT}/exec/docker_done
-  echo "/usr/local/bin/aws s3 cp /mnt/batch/output/result.tar.gz s3://scar-architrave/output/result_$(date | tr ' ' _ ).tar.gz" >> ${S3_BATCH_MNT}/exec/docker_done
-  log "Signaling children to exit"
-  cat ${HOST_FILE_PATH}-deduped | awk -F_ '{print $1}' | xargs -I{} -n1 ssh {} "touch /mnt/batch/mpi/master_done"
-  while inotifywait ${S3_BATCH_MNT}/exec -e create; do { echo "EC2 host post-execution process completed, exiting container"; break; }; done
+  #echo "env GZIP=-9 tar -czvf /mnt/batch/output/result.tar.gz /mnt/batch/output/*" > ${S3_BATCH_MNT}/exec/docker_done
+  #echo "/usr/local/bin/aws s3 cp /mnt/batch/output/result.tar.gz s3://scar-architrave/output/result_$(date | tr ' ' _ ).tar.gz" >> ${S3_BATCH_MNT}/exec/docker_done
+  #log "Signaling children to exit"
+  #cat ${HOST_FILE_PATH}-deduped | awk -F_ '{print $1}' | xargs -I{} -n1 ssh {} "touch /mnt/batch/mpi/master_done"
+  #while inotifywait ${S3_BATCH_MNT}/exec -e create; do { echo "EC2 host post-execution process completed, exiting container"; break; }; done
   exit 0
 }
 
@@ -122,10 +122,10 @@ report_to_master () {
   do
     echo "Sleeping 5 seconds and trying again"
   done
-  touch ${S3_BATCH_MNT}/exec/docker_done
+  #touch ${S3_BATCH_MNT}/exec/docker_done
 
-  echo "Wait for master to finish"
-  while inotifywait ${S3_BATCH_MNT}/mpi -e create; do { echo "Master has finished its execution, done! goodbye"; break; }; done
+  #echo "Wait for master to finish"
+  #while inotifywait ${S3_BATCH_MNT}/mpi -e create; do { echo "Master has finished its execution, done! goodbye"; break; }; done
   exit 0
 }
 
