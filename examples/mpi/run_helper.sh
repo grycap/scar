72366f95f32d265dd175f992f30c14751591c8c7
asalic <asalic@users.noreply.github.com>
2021-03-10 11:09:16 +0100

    rm general mpi example

--- examples/mpi/run_helper.sh
+++
@@ -1,8 +0,0 @@
-# Uncomment AWS_BATCH_JOB_NUM_NODES,AWS_BATCH_JOB_NODE_INDEX, and AWS_BATCH_JOB_MAIN_NODE_INDEX when running batch on single node
-# export AWS_BATCH_JOB_NUM_NODES=1
-# export AWS_BATCH_JOB_NODE_INDEX=0
-# export AWS_BATCH_JOB_MAIN_NODE_INDEX=0
-
-export APP_PARAMS=''
-export APP_BIN=''
-bash /opt/run.sh
