f8a96791cf55aecd7991525eebe8d7e93cf831cc
micafer <micafer1@upv.es>
2022-01-04 16:42:38 +0100

    Add tests

--- test/unit/aws/test_ecr.py
+++ test/unit/aws/test_ecr.py
@@ -1,9 +1,22 @@
+#! /usr/bin/python
 
+# Copyright (C) GRyCAP - I3M - UPV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import unittest
 import sys
 from mock import MagicMock
 from mock import patch, call
-
 
 sys.path.append("..")
 sys.path.append(".")
@@ -11,11 +24,11 @@ sys.path.append("../..")
 
 from scar.providers.aws.ecr import ECR
 
-class TestECRC(unittest.TestCase):
+
+class TestECR(unittest.TestCase):
 
     def __init__(self, *args):
         unittest.TestCase.__init__(self, *args)
-
 
     def test_init(self):
         ecr = ECR({})
@@ -52,3 +65,24 @@ class TestECRC(unittest.TestCase):
         ecr.client.client.describe_repositories.return_value = {'repositories': [{'repositoryUri': 'URI'}]}
         self.assertEqual(ecr.get_repository_uri('repo_name'), 'URI')
         self.assertEqual(ecr.client.client.describe_repositories.call_args_list[0], call(repositoryNames=['repo_name']))
+
+    @patch('boto3.Session')
+    def test_create_repository(self, boto_session):
+        session = MagicMock(['client'])
+        client = MagicMock(['create_repository'])
+        session.client.return_value = client
+        boto_session.return_value = session
+        ecr = ECR({})
+        ecr.client.client.create_repository.return_value = {'repository': {'repositoryUri': 'URI'}}
+        self.assertEqual(ecr.create_repository('repo_name'), 'URI')
+        self.assertEqual(ecr.client.client.create_repository.call_args_list[0], call(repositoryName='repo_name'))
+
+    @patch('boto3.Session')
+    def test_delete_repository(self, boto_session):
+        session = MagicMock(['client'])
+        client = MagicMock(['delete_repository'])
+        session.client.return_value = client
+        boto_session.return_value = session
+        ecr = ECR({})
+        ecr.delete_repository('repo_name')
+        self.assertEqual(ecr.client.client.delete_repository.call_args_list[0], call(repositoryName='repo_name', force=True))
---
+++ test/unit/aws/test_s3.py
@@ -0,0 +1,67 @@
+#! /usr/bin/python
+
+# Copyright (C) GRyCAP - I3M - UPV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+import sys
+import tempfile
+import os.path
+from mock import MagicMock
+from mock import patch, call
+from botocore.exceptions import ClientError
+
+sys.path.append("..")
+sys.path.append(".")
+sys.path.append("../..")
+
+from scar.providers.aws.s3 import S3
+
+
+class TestS3(unittest.TestCase):
+
+    def __init__(self, *args):
+        unittest.TestCase.__init__(self, *args)
+
+    def test_init(self):
+        ecr = S3({})
+        self.assertEqual(type(ecr.client.client).__name__, "S3")
+
+    @patch('boto3.Session')
+    def test_create_bucket(self, boto_session):
+        session = MagicMock(['client'])
+        client = MagicMock(['get_bucket_location', 'create_bucket'])
+        session.client.return_value = client
+        boto_session.return_value = session
+        s3 = S3({})
+        s3.client.client.get_bucket_location.side_effect = ClientError({'Error': {'Code': 'NoSuchBucket'}}, 'op')
+        s3.client.client.create_bucket.return_value = {}
+        s3.create_bucket('bname')
+        self.assertEqual(s3.client.client.create_bucket.call_args_list[0], call(ACL='private', Bucket='bname'))
+
+    @patch('boto3.Session')
+    def test_upload_file(self, boto_session):
+        session = MagicMock(['client'])
+        client = MagicMock(['put_object'])
+        session.client.return_value = client
+        boto_session.return_value = session
+        s3 = S3({})
+        s3.client.client.put_object.return_value = {}
+        tmpfile = tempfile.NamedTemporaryFile(delete=False)
+        tmpfile.write(b'Hello world!')
+        tmpfile.close()
+        s3.upload_file('bname', file_path=tmpfile.name)
+        os.unlink(tmpfile.name)
+        self.assertEqual(s3.client.client.put_object.call_args_list[0],
+                         call(Bucket='bname', Key=os.path.basename(tmpfile.name), Body=b'Hello world!'))
+
