908d142c8a1650105987087da95c6c81bdfbaecc
micafer <micafer1@upv.es>
2022-01-11 16:07:30 +0100

    Add tests

--- test/unit/aws/test_cloudwatchlogs.py
+++ test/unit/aws/test_cloudwatchlogs.py
@@ -31,12 +31,12 @@ class TestCloudWatchLogs(unittest.TestCase):
         unittest.TestCase.__init__(self, *args)
 
     def test_init(self):
-        ecr = CloudWatchLogs({})
-        self.assertEqual(type(ecr.client.client).__name__, "CloudWatchLogs")
+        cwl = CloudWatchLogs({})
+        self.assertEqual(type(cwl.client.client).__name__, "CloudWatchLogs")
 
     def test_get_log_group_name(self):
-        iam = CloudWatchLogs({'lambda': {'name': 'fname'}})
-        self.assertEqual(iam.get_log_group_name(), '/aws/lambda/fname')
+        cwl = CloudWatchLogs({'lambda': {'name': 'fname'}})
+        self.assertEqual(cwl.get_log_group_name(), '/aws/lambda/fname')
 
     @patch('boto3.Session')
     def test_create_log_group(self, boto_session):
@@ -44,12 +44,12 @@ class TestCloudWatchLogs(unittest.TestCase):
         client = MagicMock(['create_log_group', 'put_retention_policy'])
         session.client.return_value = client
         boto_session.return_value = session
-        iam = CloudWatchLogs({'lambda': {'name': 'fname', 'tags': {'createdby': 'scar'}},
+        cwl = CloudWatchLogs({'lambda': {'name': 'fname', 'tags': {'createdby': 'scar'}},
                               'cloudwatch': {'log_retention_policy_in_days': 1}})
-        iam.client.client.create_log_group.return_value = "resp"
-        self.assertEqual(iam.create_log_group(), "resp")
+        cwl.client.client.create_log_group.return_value = "resp"
+        self.assertEqual(cwl.create_log_group(), "resp")
         res = {'logGroupName': '/aws/lambda/fname', 'tags': {'createdby': 'scar'}}
-        self.assertEqual(iam.client.client.create_log_group.call_args_list[0][1], res)
+        self.assertEqual(cwl.client.client.create_log_group.call_args_list[0][1], res)
 
     @patch('boto3.Session')
     def test_get_aws_logs(self, boto_session):
@@ -57,9 +57,9 @@ class TestCloudWatchLogs(unittest.TestCase):
         client = MagicMock(['filter_log_events', 'describe_jobs'])
         session.client.return_value = client
         boto_session.return_value = session
-        iam = CloudWatchLogs({'lambda': {'name': 'fname'},
+        cwl = CloudWatchLogs({'lambda': {'name': 'fname'},
                               'cloudwatch': {'log_stream_name': 'stream',
                                              'request_id': 'reqid'}})
-        iam.client.client.filter_log_events.return_value = {'events': [{'message': 'mess', 'timestamp': 'times'}]}
-        iam.client.client.describe_jobs.return_value = {'jobs': [{'status': 'SUCCEEDED'}]}
-        self.assertEqual(iam.get_aws_logs(), "Batch job status: SUCCEEDED\nmess")
+        cwl.client.client.filter_log_events.return_value = {'events': [{'message': 'mess', 'timestamp': 'times'}]}
+        cwl.client.client.describe_jobs.return_value = {'jobs': [{'status': 'SUCCEEDED'}]}
+        self.assertEqual(cwl.get_aws_logs(), "Batch job status: SUCCEEDED\nmess")
