3c18f74d294f8cf1e0334d6fa78a4dee6827179d
micafer <micafer1@upv.es>
2022-01-25 16:56:27 +0100

    Implements #364

--- scar/providers/aws/lambdafunction.py
+++ scar/providers/aws/lambdafunction.py
@@ -65,6 +65,8 @@ class Lambda(GenericClient):
                 'MemorySize': self.function.get('memory'),
                 'Tags': self.function.get('tags'),
                 'Architectures': self.function.get('architectures', ['x86_64'])}
+        if self.function.get('vpc'):
+            args['VpcConfig'] = self.function.get('vpc')
         if self.function.get('runtime') == "image":
             args['Code'] = {'ImageUri': self.function.get('container').get('image')}
             args['PackageType'] = 'Image'
